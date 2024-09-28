2350b531ea9d37afc5095310fc970ff550ce3ddd
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-09-28 17:47:55 +0700

    feat: add nextui input

--- src/app/product/[slug]/components/checkout-form/index.tsx
+++ src/app/product/[slug]/components/checkout-form/index.tsx
@@ -1,9 +1,31 @@
+'use client';
+
 import React from 'react'
+import { Input, Textarea } from "@nextui-org/input";
 
 const CheckoutForm = () => {
    return (
-      <div>
-         <h2>Checkout form</h2>
+      <div className='bg-gray-50 p-4 rounded-md'>
+         <h2 className='font-semibold text-xl'>Checkout form</h2>
+         <div className='flex flex-col gap-3'>
+            <Input size='sm' variant='underlined' label="Your name" />
+            <Input size='sm' variant='underlined' label="Your whatsapp number" />
+            <Input size='sm' variant='underlined' label="City" />
+            <Textarea
+               size='sm'
+               variant='underlined'
+               label="Address"
+               placeholder="Enter your Address"
+            />
+            <Input size='sm' variant='underlined' label="Purchase amount" />
+            <Input size='sm' variant='underlined' label="Email" />
+            <Textarea
+               size='sm'
+               variant='underlined'
+               label="Notes"
+               placeholder="Enter your Notes"
+            />
+         </div>
       </div>
    )
 }
