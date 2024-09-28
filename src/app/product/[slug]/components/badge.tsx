788858e58e7efbc1039dffccc54297b00206cba3
Ruslan Wanandi <devruslan03@gmail.com>
2024-09-28 09:01:39 +0700

    fix: badge contrast color

--- src/app/product/[slug]/components/badge.tsx
+++ src/app/product/[slug]/components/badge.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Badge: React.FC<{ children: React.ReactNode }> = ({ children }) => {
    return (
-      <div className='px-2 text-sm rounded-md bg-yellow-400 text-white'>{children}</div>
+      <div className='px-2 text-sm font-semibold rounded-md bg-yellow-400'>{children}</div>
    )
 }
 
