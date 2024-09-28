c8979244a4adf0562c1c47d283e19b013001c5b2
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-09-28 21:49:42 +0700

    feat: try shadcn ui

--- src/app/product/[slug]/components/checkout-form/index.tsx
+++ src/app/product/[slug]/components/checkout-form/index.tsx
@@ -1,28 +1,24 @@
 'use client';
 
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 import React from 'react'
-import { Input, Textarea } from "@nextui-org/input";
 
 const CheckoutForm = () => {
    return (
       <div className='bg-gray-50 p-4 rounded-md'>
          <h2 className='font-semibold text-xl'>Checkout form</h2>
          <div className='flex flex-col gap-3'>
-            <Input size='sm' variant='underlined' label="Your name" />
-            <Input size='sm' variant='underlined' label="Your whatsapp number" />
-            <Input size='sm' variant='underlined' label="City" />
+            <Input type='text' placeholder="Email" />
+
+            <Input placeholder="Your whatsapp number" />
+            <Input placeholder="City" />
             <Textarea
-               size='sm'
-               variant='underlined'
-               label="Address"
-               placeholder="Enter your Address"
+               placeholder="Address"
             />
-            <Input size='sm' variant='underlined' label="Purchase amount" />
-            <Input size='sm' variant='underlined' label="Email" />
+            <Input placeholder="Purchase amount" />
+            <Input placeholder="Email" />
             <Textarea
-               size='sm'
-               variant='underlined'
-               label="Notes"
                placeholder="Enter your Notes"
             />
          </div>
