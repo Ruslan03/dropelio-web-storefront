7f9342ab71b6ff45e253b83a412651e1255fbaec
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-09-29 12:37:59 +0700

    feat: update ui

--- src/app/product/[slug]/components/features.tsx
+++ src/app/product/[slug]/components/features.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Features = ({ features }: { features: string[] }) => {
    return (
-      <div className='bg-yellow-50 rounded-md p-3 px-4'>
-         <ul className='flex flex-col gap-1'>
-            {(features || []).map((feature, i) => <li key={i}>&#10004; {feature}</li>)}
-         </ul>
-      </div>
+      <ul className='flex flex-col gap-1'>
+         {(features || []).map((feature, i) => <li key={i}>&#10004; {feature}</li>)}
+      </ul>
    )
 }
 
