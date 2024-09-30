276a216c7da012373caaeb554012f90f4baaf840
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-09-30 17:16:21 +0700

    feat: font cairo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from "next";
-
+import { Cairo, Inter } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 
 import "./globals.css";
 import { getStore } from "./lib/services";
+
+
+const cairo = Cairo({
+   weight: ['400', '700'],
+   style: ['normal'],
+   subsets: ['latin'],
+   display: 'swap',
+})
 
 export default async function RootLayout({
    children,
@@ -23,7 +30,9 @@ export default async function RootLayout({
       <html lang={locale} dir={dir}>
          <body>
             <NextIntlClientProvider messages={messages}>
-               {children}
+               <main className={locale === 'ar' ? cairo.className: ''}>
+                  {children}
+               </main>
             </NextIntlClientProvider>
          </body>
       </html>
