6b36f69ee6f36af9216bc892c1b990a574759a62
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-10-03 23:49:37 +0700

    feat: touchup footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -9,12 +9,8 @@ interface BaseFooter {
 const BaseFooter: React.FC<BaseFooter> = () => {
    return (
       <div className='flex flex-col items-center justify-center h-32 border-t border-opacity-0'>
-         <ul className='flex gap-4 items-center text-sm'>
-            <li className='hover:underline'><Link href={'/term-of-service'}>Term of Services</Link></li>
-            <li className='hover:underline'><Link href={'/privacy-policy'}>Privacy Policy</Link></li>
-            <li className='hover:underline'><Link href={'/refund-policy'}>Refund Policy</Link></li>
-         </ul>
-         <p className='text-xs mt-3'>Powered By:</p>
+         
+         <p className='text-xs'>Powered By:</p>
          <Link href={'https://dropelio.shop'} target='_blank'>
             <Image
                src={'/dropelio-logo.png'}
@@ -28,6 +24,12 @@ const BaseFooter: React.FC<BaseFooter> = () => {
                }}
             />
          </Link>
+
+         <ul className='flex gap-4 sm:gap-8 items-center text-sm mt-3'>
+            <li className='hover:underline'><Link href={'/term-of-service'}>Term of Services</Link></li>
+            <li className='hover:underline'><Link href={'/privacy-policy'}>Privacy Policy</Link></li>
+            <li className='hover:underline'><Link href={'/refund-policy'}>Refund Policy</Link></li>
+         </ul>
       </div>
    )
 }
