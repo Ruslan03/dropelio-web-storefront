35e1bedb1cdccd7be8566b4e1e7a104e77b1bbde
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-10-03 11:42:50 +0700

    feat: loading placeholder

--- src/app/components/product-list.tsx
+++ src/app/components/product-list.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { getProducts } from '../lib/services'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -9,47 +9,75 @@ import BadgeRating from './badge-rating'
 import Price from './price'
 
 const ProductList = () => {
+   const [isLoading, setIsLoading] = useState(true)
    const [page, setPage] = useState(1)
    const [products, setProducts] = useState<any[]>([])
 
-   useEffect(() => {
-      getProducts({ page }).then((resProducts: any) => {
-         setProducts((prevProducts) => ([...prevProducts, ...(resProducts?.data || [])]))
-      })
+   const fetchProduct = useCallback(() => {
+      setIsLoading(true)
+      getProducts({ page })
+         .then((resProducts: any) => {
+            setProducts((prevProducts) => ([...prevProducts, ...(resProducts?.data || [])]))
+         })
+         .finally(() => setIsLoading(false))
    }, [page])
 
-   return (
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 min-h-96">
-         {products.map((product) => (
-            <Link key={product?.id} href={`${product?.slug}`}
-               className="relative w-full aspect-square bg-cover group rounded-2xl md:rounded-3xl bg-center overflow-hidden mx-auto sm:mr-0 xl:mx-auto active:opacity-75 transition-all ease-in-out">
-               {/* <img className="md:rounded-2xl object-cover" src="https://pagedone.io/asset/uploads/1700731972.png" alt="Jacket image" /> */}
+   useEffect(() => {
+      fetchProduct()
+   }, [page, fetchProduct])
 
-               <div className='w-full h-full relative'>
-                  <Image
-                     src={baseUrl(`storage/${product?.product_images?.[0].image_path}`)}
-                     alt={product?.title}
-                     fill
-                     priority
-                     // onLoad={() => setIsLoading(false)}
-                     style={{
-                        objectFit: 'cover',
-                     }}
-                  />
-               </div>
-               <div
-                  className="absolute z-10 bottom-3 left-0 mx-3 p-3 bg-white w-[calc(100%-24px)] rounded-xl md:rounded-2xl shadow-md">
-                  <h1 className="font-semibold text-base md:text-sm text-black line-clamp-1">{product?.title}</h1>
-                  <div className="flex items-center justify-between mt-3 md:mt-2">
-                     <Price currency={product?.product_currency} price={product?.product_price_formatted} compare={product?.compare_at_price_formatted} />
-                     <BadgeRating rating={product?.rating} />
-                     {/* <h6 className="font-semibold text-base leading-7 text-indigo-600 text-right">$100</h6> */}
-                  </div>
-               </div>
-            </Link>
+
+   return (
+      <div>
+         {!isLoading && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 min-h-96">
+               {products.map((product) => (
+                  <Link key={product?.id} href={`${product?.slug}`}
+                     className="relative w-full aspect-square bg-cover group rounded-2xl sm:rounded-3xl bg-center overflow-hidden mx-auto sm:mr-0 xl:mx-auto active:opacity-75 transition-all ease-in-out">
+
+                     <div className='w-full h-full relative'>
+                        <Image
+                           src={baseUrl(`storage/${product?.product_images?.[0].image_path}`)}
+                           alt={product?.title}
+                           fill
+                           priority
+                           // onLoad={() => setIsLoading(false)}
+                           style={{
+                              objectFit: 'cover',
+                           }}
+                        />
+                     </div>
+                     <div
+                        className="absolute z-10 bottom-3 left-0 mx-3 p-3 bg-white w-[calc(100%-24px)] rounded-xl sm:rounded-2xl shadow-md">
+                        <h1 className="font-semibold text-base sm:text-sm text-black line-clamp-1">{product?.title}</h1>
+                        <div className="flex items-center justify-between mt-3 sm:mt-2">
+                           <Price currency={product?.product_currency} price={product?.product_price_formatted} compare={product?.compare_at_price_formatted} />
+                           <BadgeRating rating={product?.rating} />
+                        </div>
+                     </div>
+                  </Link>
+               ))}
+
+            </div>
+         )}
+         {isLoading && (
+            <LoadingPlaceholder />
+         )}
+      </div>
+   )
+}
+
+const LoadingPlaceholder = () => {
+   return (
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
+         {Array.from(new Array(4)).map((_, i) => (
+            <div key={i} className="relative w-full aspect-square bg-gray-100 rounded-2xl sm:rounded-3xl sm:mr-0 xl:mx-auto active:opacity-75 animate-pulse transition-all ease-in-out">
+            </div>
          ))}
 
       </div>
+
    )
 }
 
