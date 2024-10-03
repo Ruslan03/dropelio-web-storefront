07cf3b0bf1ead2138e2b29c402c06429b7dc5e72
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-10-03 11:45:17 +0700

    fix: user scalable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const viewport: Viewport = {
    width: 'device-width',
    initialScale: 1,
    maximumScale: 1,
-   userScalable: false,
+   userScalable: true,
 }
 
 export default async function RootLayout({
