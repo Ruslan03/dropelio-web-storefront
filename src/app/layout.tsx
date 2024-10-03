9015cd77be891d3666ec71f5f0e6ca98eebdb985
Ruslan Wanandi <ruslanwanandi@gmail.com>
2024-10-03 11:54:39 +0700

    fix: maximum scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const cairo = Cairo({
 export const viewport: Viewport = {
    width: 'device-width',
    initialScale: 1,
-   maximumScale: 1,
+   maximumScale: 2,
 }
 
 export default async function RootLayout({
